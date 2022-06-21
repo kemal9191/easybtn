a452828e2221603b3e298752473d9f99162cc864
kemal9191 <kpehlivan91@gmail.com>
2022-06-21 11:13:24 +0300

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'easybtn'
+import { Button } from 'easybtn'
 import 'easybtn/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <>
+<Button type="primary" />
+  <Button type="secondary" />
+  <Button type="danger" />
+  <Button type="warning" />
+</>
 }
 
 export default App
